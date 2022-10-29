e6deed1b52b1353115fc2a06ae37cb10e9abc5b4
frank jones <frank@frankjones.co>
2022-10-29 11:42:55 +0200

    Navbar showing logged in user image, greeting and dropdown. User menu items right aligned.

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,7 +13,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
+            <div class="navbar-nav w-100">
                 <a class="nav-link {{ (request()->segment(1) == null) ? 'active' : null }}" aria-current="page" href="/">Home</a>
                 <a class="nav-link {{ (request()->segment(1) == 'about') ? 'active' : null }}" href="/about">About</a>
                 <a class="nav-link {{ (request()->segment(1) == 'articles') ? 'active' : null }}" href="/articles">Articles</a>
@@ -21,22 +21,47 @@
                 <a class="nav-link {{ (request()->segment(1) == 'users') ? 'active' : null }}" href="/users">Users</a>
                 <a class="nav-link {{ (request()->segment(1) == 'contact') ? 'active' : null }}" href="/contact">Contact</a>
                 
-                @auth
-                    <a class="nav-link {{ (Route::currentRouteName() == 'dashboard') ? 'active' : null }}" href="/dashboard">Dashboard</a>
-                    <a class="nav-link {{ (Route::currentRouteName() == 'profile') ? 'active' : null }}" href="/profile">Profile</a>
-                    <form action="/logout" class="inline"  id="logoutForm" method="POST">
-                        @csrf
-                        <a class="nav-link" onclick="logoutSubmit()" style="cursor:pointer;">Log out</a>
-                    </form>
-                    <script>
-                        function logoutSubmit(){
-                            document.getElementById("logoutForm").submit();
-                        }
-                    </script>
-                @else
-                    <a class="nav-link {{ (Route::currentRouteName() == 'register') ? 'active' : null }}" href="/register">Register</a>
-                    <a class="nav-link {{ (Route::currentRouteName() == 'login') ? 'active' : null }}" href="/login">Login</a>
-                @endauth
+                <div class="d-flex ms-auto">
+                    @auth
+                        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <img 
+                                            src="{{asset('images/users/'.auth()->user()->hex.'/tn-'.auth()->user()->image)}}" 
+                                            alt="" 
+                                            class="nav-user-image"
+                                            style="border-color: #{{auth()->user()->color->code}};"    
+                                        >
+                                        Hello {{auth()->user()->first_name}}
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                        <li>
+                                            <a class="nav-link {{ (Route::currentRouteName() == 'profile') ? 'active' : null }}" href="/profile">Profile</a>
+                                        </li>
+                                        <li>
+                                            <a class="nav-link {{ (Route::currentRouteName() == 'dashboard') ? 'active' : null }}" href="/dashboard">Dashboard</a>
+                                        </li>
+                                        <li>
+                                            <form action="/logout" class="inline"  id="logoutForm" method="POST">
+                                                @csrf
+                                                <a class="nav-link" onclick="logoutSubmit()" style="cursor:pointer;">Log out</a>
+                                            </form>
+                                            <script>
+                                                function logoutSubmit(){
+                                                    document.getElementById("logoutForm").submit();
+                                                }
+                                            </script>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    @else
+                        <a class="nav-link {{ (Route::currentRouteName() == 'register') ? 'active' : null }}" href="/register">Register</a>
+                        <a class="nav-link {{ (Route::currentRouteName() == 'login') ? 'active' : null }}" href="/login">Login</a>
+                    @endauth
+                </div>
 
             </div>
         </div>
