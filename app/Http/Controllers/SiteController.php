b4214849cd3a6ae77698cabb8a2f6dc80e314464
frank jones <frank@frankjones.co>
2022-11-04 16:47:34 +0100

    SavePoint before big Refactor

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,15 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Article;
-use App\Models\Sponsor;
-use App\Models\Category;
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Session;
 
 class SiteController extends Controller
 {
